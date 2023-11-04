32f65b41cf66762ec142dd4dfa05f04ca740a4fb
manisandi <149861886+manisandi@users.noreply.github.com>
2023-11-04 01:31:15 -0600

    test

--- PYTfinal/src/pytfinal/PYTfinal.java
+++ PYTfinal/src/pytfinal/PYTfinal.java
@@ -14,7 +14,9 @@ public class PYTfinal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
+    
+    public void suMadre (){}
     
 }
